4abe981725a1e7ada9e7e60be2d01d9c9ba27a07
EJKim <dmdwn3979@gmail.com>
2022-06-07 21:53:29 +0900

    Fix: 회원가입 페이지

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './index.css'
 import { Link } from "react-router-dom";
+import kakaoBtn from '../../assets/img/kakao_login_large_wide.png'
+
 const LoginPage = () =>{
 
     return (
         <div className='loginPage'>
             <h1>WebSocket Chrome Extension Test Front Page</h1>
-            <input className='input_holder' placeholder='아이디를 입력해주세요' />
+            <input className='input_holder' type='text' placeholder='아이디를 입력해주세요' />
             <br />
             <br />
-            <input className='input_holder' placeholder='비밀번호를 입력해주세요'/>
+            <input className='input_holder' type='password' placeholder='비밀번호를 입력해주세요'/>
             <br />
             <br />
             <button className='login_btn'>로그인</button>
+            <br />
+            <a href={'http://localhost:8080/oauth2/authorization/kakao'}><img className='login_btn_kakao' src={kakaoBtn} /></a>
             <br />
             <div className='login_extra'>
             <Link to='/signup'>회원가입</Link> / <a>아이디 찾기</a> / <a>비밀번호 찾기</a>
--- src/components/SignupPage/index.js
+++ src/components/SignupPage/index.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css'
-import kakaoBtn from '../../assets/img/kakao_login_large_wide.png'
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`
+import { signUp } from '../../api/sign';
 const SignUpPage = () =>{
+    const [userInfo, setUserInfo] = useState({ email: '', nickname: '', password: ''});
+    
+    const onUserInfoChange = (target, e) => {
+        if(target === 'USER_ID'){
+            setUserInfo((prevState) => { return {...prevState, email: e.target.value}})
+        }
+        else if('USER_NICKNAME'){
+            setUserInfo((prevState) => { return {...prevState, nickname: e.target.value}})
+        }
+        else if(target === 'USER_PASSWORD'){
+            setUserInfo((prevState) => { return {...prevState, password: e.target.value}})
+        }
 
+    }
+
+    const handleSignup = () => {
+        console.log(userInfo)
+        signUp(userInfo);
+    }
     return (
         <div className='signupPage'>
             <h1>WebSocket Chrome Extension Test Sign Up</h1>
-            <a href={KAKAO_AUTH_URL}><img src={kakaoBtn} /></a>
+            <br />
+            <br />
+            <input className='input_holder' type='text' placeholder='닉네임을 입력해주세요' onChange={(e)=>onUserInfoChange('USER_NICKNAME', e)}/>
+            <br />
+            <br />
+            <input className='input_holder' type='text' placeholder='아이디를 입력해주세요' onChange={(e)=>onUserInfoChange('USER_ID', e)}/>
+            <br />
+            <br />
+            <input className='input_holder' type='password' placeholder='비밀번호를 입력해주세요' onChange={(e)=>onUserInfoChange('USER_PASSWORD', e)}/>
+            <br />
+            <br />
+            <button className='signup_btn' onClick={handleSignup}>회원가입</button>
+            <br />
         </div>
     )
 }
