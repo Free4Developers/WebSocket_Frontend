aefe8825d646dbe716f6997972e2c9704e08c542
EJKim <dmdwn3979@gmail.com>
2022-06-08 15:44:51 +0900

    Add: 로그인 -> 메인페이지 연동

--- src/components/RedirectPage/index.js
+++ src/components/RedirectPage/index.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { getUserToken } from "../../api/sign";
 import { useDispatch } from 'react-redux';
 import { authActions } from '../../store/index';
-
+import { useNavigate } from 'react-router-dom';
 const RedirectPage = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const params = new URLSearchParams(window.location.search)
-    getUserToken(params.get('code'), params.get('state')).then((res)=>dispatch(authActions.login(res)));
+    getUserToken(params.get('code'), params.get('state')).then((res) => dispatch(authActions.login(res)));
+    useEffect(()=>{
+        navigate('/');
+    },[])
 
-    return(<div>
-        hello
-    </div>);
+    return
 }
 
 export default React.memo(RedirectPage);
