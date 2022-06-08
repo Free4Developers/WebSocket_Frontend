2d7ef15d14e002817c2bb437f24b2e78e5b4b720
EJKim <dmdwn3979@gmail.com>
2022-06-08 15:45:03 +0900

    Add: 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import LoginPage from  './components/LoginPage/index'
 import SignUpPage from './components/SignupPage';
 import RedirectPage from './components/RedirectPage';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LoginPage />} />
+        <Route path='/' element={<MainPage />} />
+        <Route path='/login' element={<LoginPage />} />
         <Route path ='/signup' element={<SignUpPage />} />
         <Route path ='/oauth2/login/callback/kakao' element={<RedirectPage />} />
       </Routes>
